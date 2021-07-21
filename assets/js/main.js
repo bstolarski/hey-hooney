0be08baf7487f63f0f4d040a8cd82e7369797cc2
Bartłomiej Stolarski <bartlomiej.stolarski@MacBook-Pro-Bartomiej-Stolarski.local>
2021-07-21 13:19:34 +0200

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
+// function loadJSON(callback) {   
 
+//   const xobj = new XMLHttpRequest();
+//       xobj.overrideMimeType("application/json");
+//   xobj.open('GET', './data.json', true); // Replace 'my_data' with the path to your file
+//   xobj.onreadystatechange = function () {
+//         if (xobj.readyState == 4 && xobj.status == "200") {
+//           // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+//           callback(JSON.parse(xobj.responseText));
+//         }
+//   };
+//   xobj.send(null);  
+// }
+// console.log(loadJSON());
 
 // store filter
 
 const allCheckboxes = document.querySelectorAll('input[type=checkbox]');
 const allProducts = Array.from(document.querySelectorAll('.store__data'));
-const checked = {};
 
 Array.prototype.forEach.call(allCheckboxes, function (el) {
     el.addEventListener('change', toggleCheckbox);
@@ -12,15 +24,12 @@ Array.prototype.forEach.call(allCheckboxes, function (el) {
 
 function toggleCheckbox(e) {
     e.target.classList.toggle('checked');
-    setVisibility();
+    setVisibility(e);
 }  
 
-function setVisibility() {
+function setVisibility(e) {
     allProducts.map(function (el) {
-      const wielokwiatowy = e.target.classList.contains('checked"') ? el.classList.contains('wielokwiatowy') : true;
-      const lipowy = e.target.classList.contains('checked"') ? el.classList.contains('lipowy') : true;
-      const spadziowy = e.target.classList.contains('checked"') ? el.classList.contains('spadziowy') : true;
-      if (wielokwiatowy && lipowy && spadziowy) {
+      if (e.target.classList.contains('checked') && el.classList.contains(e.target.value)) {
         el.style.display = 'block';
       } else {
         el.style.display = 'none';
