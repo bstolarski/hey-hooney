2db95feb0f7f5e6cba539fcc2dcc23a37344b25e
Bartłomiej Stolarski <bartlomiej.stolarski@ANGELINIA-T440.intranet.fater.it>
2021-07-27 17:04:36 +0200

    Form Validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,55 @@ function setVisibility(e) {
 // reveal.reveal('.store__data', {
 //   delay: 1000
 // },1000);
+
+// FORM VALIDATION
+const form = document.querySelector("form[name='contact-form']");
+const thankYou = document.querySelector(".hidden");
+const nameInput = document.querySelector("input[name='name']");
+const emailInput = document.querySelector("input[name='email']");
+const messageInput = document.querySelector("textarea[name='message']");
+
+nameInput.isValid = () => !!nameInput.value;
+emailInput.isValid = () => isValidEmail(emailInput.value);
+messageInput.isValid = () => !!messageInput.value;
+
+const inputFields = [nameInput, emailInput, messageInput];
+
+const isValidEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+let shouldValidate = false;
+let isFormValid = false;
+
+const validateInputs = () => {
+  console.log("we are here");
+  if (!shouldValidate) return;
+
+  isFormValid = true;
+  inputFields.forEach((input) => {
+    input.classList.remove("invalid");
+    input.nextElementSibling.classList.add("hide");
+
+    if (!input.isValid()) {
+      input.classList.add("invalid");
+      isFormValid = false;
+      input.nextElementSibling.classList.remove("hide");
+    }
+  });
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  shouldValidate = true;
+  validateInputs();
+  if (isFormValid) {
+    // TODO: DO REQUEST
+    form.classList.add("hidden");
+    thankYou.classList.remove("hidden");
+    thankYou.classList.add("thank-you");
+  }
+});
+
+inputFields.forEach((input) => input.addEventListener("input", validateInputs));
